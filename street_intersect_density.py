f0fd7295fd372b848a6ca563b216491296a86a5b
Ehsan Najafi <142501937+ehsan-najafi@users.noreply.github.com>
2025-02-26 14:42:38 +0330

    Update street_intersect_density.py

--- street_intersect_density.py
+++ street_intersect_density.py
@@ -8,9 +8,9 @@
 
 
 ### Input data
-in_sample_points = r"D:\built-environment-gis-based-algorithms\data_test\sample_data.gdb\sample_participants_home"
-in_street_fc = r"D:\built-environment-gis-based-algorithms\data_test\sample_data.gdb\sample_streets"
-in_network_dataset = r"D:\built-environment-gis-based-algorithms\data_test\sample_data.gdb\pedstrain_network_ND"
+in_sample_points = r"D:\gis-based-built-environment-algorithms\data_test\sample_data.gdb\sample_participants_home"
+in_street_fc = r"D:\gis-based-built-environment-algorithms\data_test\sample_data.gdb\sample_streets"
+in_network_dataset = r"D:\gis-based-built-environment-algorithms\data_test\sample_data.gdb\pedstrain_network_ND"
 
 buffer_in_meters = 1000
 
